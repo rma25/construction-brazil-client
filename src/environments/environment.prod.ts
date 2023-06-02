341f549dc1c14d66c8365e7c9aac5bbdf6929c46
rma25 <therodoandrade@gmail.com>
2023-06-02 14:41:05 -0400

    * Added Production environment variables;

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  version: '1.0',
+  toastColor: 'D4EDDA',
+  toastError: 'F88070',
+  constructionBrazilServerUri: 'https://constructionbrazilserverdev.azurewebsites.net/',
+  preCepUri: 'https://viacep.com.br/ws/',
+  postCepUri: '/json'
 };
